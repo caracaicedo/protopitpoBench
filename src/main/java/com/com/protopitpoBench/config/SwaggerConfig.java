f80d9cb16f733ab83a25bcc6981cc4dada12f220
caracaicedo <61522976+caracaicedo@users.noreply.github.com>
2023-10-05 15:42:29 -0500

    Update SwaggerConfig.java

--- src/main/java/com/com/protopitpoBench/config/SwaggerConfig.java
+++ src/main/java/com/com/protopitpoBench/config/SwaggerConfig.java
@@ -14,28 +14,41 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-	@Bean
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.com.protopitpoBench.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .host("https://caracaicedo.github.io/protopitpoBench"); // URL de GitHub Pages
     }
 
-	private ApiInfo apiInfo() {
-	    return new ApiInfoBuilder()
-	            .title("Api rest Banco	")
-	            .description("api rest para el el calcula de de la posicion de una nave")
-	            .termsOfServiceUrl("https://caracaicedo.github.io/protopitpoBench/swagger.json")
-	            .version("1.0")
-	            .build();
-	}
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Api rest Banco")
+                .description("api rest para el cálculo de la posición de una nave")
+                .termsOfServiceUrl("https://caracaicedo.github.io/protopitpoBench") // URL de GitHub Pages
+                .version("1.0")
+                .build();
+    }
+
+
 
 	private ApiKey apiKey() {
 	    return new ApiKey("jwtToken", "Authorization", "header");
